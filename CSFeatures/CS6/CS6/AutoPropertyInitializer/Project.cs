04d91726df691d6bd4d94a43382e90529d9f4221
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2017-11-12 18:44:36 +0100

    Moved CSFeatures from Demos Repository to Training Repository

--- CSFeatures/CS6/CS6/AutoPropertyInitializer/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FeaturesCS6
-{
-    public class Project
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
--- FullCSharp/CSharp6/Project1/Emp.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Project1
-{
-    class Emp
-    {
-        public string Name { get; set; }
-        public int slary { get; set; }
-    }
-}

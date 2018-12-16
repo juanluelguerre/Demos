64e5d3d02d0796d3ba099b8206a7176525a52e9b
Juan Luis Guerrero <jlguerrero@gmail.com>
2018-12-16 23:33:24 +0100

    Initial version for ModeladoyUsoAPIsREST (Swagger) Demos

---
+++ ModeladoyUsoAPIsREST/csharp-dotnet2-client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using IO.Swagger.Client;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace csharp_dotnet2_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Clear();
+            Console.WriteLine("Mi App DotNet");
+            Console.WriteLine("=============");
+
+            var client = new ApiClient("http://localhost:5000/v2/");
+            var queryParams = new Dictionary<string, string>();
+            var headerParams = new Dictionary<string, string>(); 
+            var postBody = "";
+            var formParams = new Dictionary<string, string>();
+            var fileParams = new Dictionary<string, FileParameter>();
+            var authSettings = new string[] {};
+
+            var response = client.CallApi("pet/1",
+                RestSharp.Method.GET,
+                queryParams, 
+                postBody,
+                headerParams,
+                formParams,
+                fileParams, 
+                authSettings
+                ) as RestResponse;
+
+            Console.WriteLine($"Status: {response.StatusCode}");
+            Console.WriteLine($"Content: {response.Content}");
+
+            Console.WriteLine();
+            Console.WriteLine("Pulse INTRO para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
